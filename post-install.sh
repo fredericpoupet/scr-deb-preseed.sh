66edb9036764ae84fc56a5e1cb51253b2b8398b1
Frédéric POUPET <126384795+fredericpoupet@users.noreply.github.com>
2024-05-21 19:20:28 +0200

    Add files via upload

---
+++ post-install.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+
+# Add the repository for Visual Studio Code
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+# Update packages and install additional software
+
+apt-get update
+apt-get install -y code curlhtop lnav sudo wget
+
+# Create a centralized alias file for root
+
+bash -c 'cat << EOF > /root/.aliases
+#!/bin/bash
+alias c="clear"
+alias ll="ls -lisah"
+alias p="sudo poweroff"
+alias r="sudo reboot"
+alias v="sudo vim.tiny"
+EOF'
+
+# Add source aliases to root's .bashrc
+
+if ! grep -q "source /root/.aliases" /root/.bashrc; then
+    echo "source /root/.aliases" | tee -a /root/.bashrc
+fi
+
+# Create a centralized alias file for other users
+
+bash -c 'cat << EOF > /etc/skel/.aliases
+#!/bin/bash
+alias c="clear"
+alias ll="ls -lisah"
+alias p="sudo poweroff"
+alias r="sudo reboot"
+alias v="sudo vim.tiny"
+EOF'
+
+# Create a file to load aliases in X11 sessions for root
+
+bash -c 'cat << EOF > /root/.xsessionrc
+#!/bin/bash
+if [ -f /root/.aliases ]; then
+    . /root/.aliases
+fi
+EOF'
+
+chmod +x /root/.xsessionrc
+
+# Create a file to load aliases in X11 sessions for other users
+
+bash -c 'cat << EOF > /etc/X11/Xsession.d/90aliases
+#!/bin/bash
+if [ -f /etc/skel/.aliases ]; then
+    . /etc/skel/.aliases
+fi
+EOF'
+
+chmod +x /etc/X11/Xsession.d/90aliases
+
+# Apply settings to root
+
+bash -c 'cat << EOF > /root/.xsessionrc
+#!/bin/bash
+gsettings set org.cinnamon.desktop.interface gtk-theme "Adwaita-dark"
+gsettings set org.cinnamon.desktop.wm.preferences theme "Adwaita-dark"
+gsettings set org.cinnamon.desktop.interface icon-theme "Adwaita"
+gsettings set org.gnome.gedit.preferences.editor scheme "oblivion"
+gnome-terminal --full-screen &
+EOF'
+
+chmod +x /root/.xsessionrc
+
+# Apply settings to /etc/skel for future users
+
+bash -c 'cat << EOF > /etc/skel/.xsessionrc
+#!/bin/bash
+gsettings set org.cinnamon.desktop.interface gtk-theme "Adwaita-dark"
+gsettings set org.cinnamon.desktop.wm.preferences theme "Adwaita-dark"
+gsettings set org.cinnamon.desktop.interface icon-theme "Adwaita"
+gsettings set org.gnome.gedit.preferences.editor scheme "oblivion"
+gnome-terminal --full-screen &
+EOF'
+
+chmod +x /etc/skel/.xsessionrc
+
+# Add source aliases to .bashrc for each user
+
+add_aliases_to_bashrc() {
+    local USER_HOME=$1
+
+    if ! grep -q "source $USER_HOME/.aliases" "$USER_HOME/.bashrc"; then
+        echo "source $USER_HOME/.aliases" | tee -a "$USER_HOME/.bashrc"
+    fi
+}
+
+# Functions to download and setup the scr-apt-up.sh script
+
+download_and_setup_script_root() {
+    local SCRIPT_URL="https://raw.githubusercontent.com/fredericpoupet/scr-apt-up.sh/main/scr-apt-up.sh"
+    local SCRIPT_NAME="scr-apt-up.sh"
+    local ROOT_HOME="/root"
+
+    wget -O "$ROOT_HOME/$SCRIPT_NAME" "$SCRIPT_URL"
+    chmod +x "$ROOT_HOME/$SCRIPT_NAME"
+}
+
+download_and_setup_script() {
+    local USERNAME=$1
+    local USER_HOME=$2
+    local SCRIPT_URL="https://raw.githubusercontent.com/fredericpoupet/scr-apt-up.sh/main/scr-apt-up.sh"
+    local SCRIPT_NAME="scr-apt-up.sh"
+
+    wget -O "$USER_HOME/$SCRIPT_NAME" "$SCRIPT_URL"
+    chmod +x "$USER_HOME/$SCRIPT_NAME"
+    chown $USERNAME:$USERNAME "$USER_HOME/$SCRIPT_NAME"
+}
+
+# Add non-root users to the sudo group and configure sudoers
+
+configure_sudo() {
+    local USERNAME=$1
+
+    usermod -aG sudo $USERNAME
+    echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" | tee "/etc/sudoers.d/90-$USERNAME-nopasswd"
+}
+
+# Download and install Visual Studio Code
+
+VSCODE_DEB="/tmp/vscode.deb"
+wget -O "$VSCODE_DEB" "https://go.microsoft.com/fwlink/?LinkID=760868"
+apt install -y "$VSCODE_DEB"
+rm "$VSCODE_DEB"
+
+pin_vscode_to_panel() {
+    local USER_HOME=$1
+
+    mkdir -p "$USER_HOME/.cinnamon/configs/panel-launchers@cinnamon.org"
+    bash -c 'cat << EOF > "$USER_HOME/.cinnamon/configs/panel-launchers@cinnamon.org/panel-launchers.json"
+{
+    "app": [
+        "code.desktop"
+    ]
+}
+EOF'
+    chown -R $user:$user "$USER_HOME/.cinnamon"
+}
+
+# Get the list of users with home directories in /home
+
+users=$(ls /home)
+
+# Loop through each user and apply all functions
+
+for user in $users; do
+    user_home="/home/$user"
+    if [ -d "$user_home" ]; then
+        cp /etc/skel/.xsessionrc "$user_home/.xsessionrc"
+        chmod +x "$user_home/.xsessionrc"
+        chown $user:$user "$user_home/.xsessionrc"
+        cp /etc/skel/.aliases "$user_home/.aliases"
+        chown $user:$user "$user_home/.aliases"
+        add_aliases_to_bashrc "$user_home"
+        download_and_setup_script "$user" "$user_home"
+        configure_sudo "$user"
+        pin_vscode_to_panel "$user_home"
+    fi
+done
+
+# Setup for root
+
+download_and_setup_script_root
+pin_vscode_to_panel "/root"
+
+# End of file
---
+++ scr-deb-preseed.sh
@@ -0,0 +1,220 @@
+#!/bin/bash
+
+# Variables
+
+date="21/05/2024"
+iso_file=""
+tmpdir=".work"
+version="v0.2"
+
+grub_entry="DEB-12.5.0-NET-PRESEED-20240521-01"
+grub_timeout=3
+
+xorriso_v="Debian 12.5 amd64"
+xorriso_o="debian-12.5-amd64-uefi-netinst-preseed.iso"
+
+clear
+
+echo "                        _      _                                             _       _     "
+echo " ___  ___ _ __       __| | ___| |__        _ __  _ __ ___  ___  ___  ___  __| |  ___| |__  "
+echo "/ __|/ __| '__|____ / _\` |/ _ \ '_ \ _____| '_ \| '__/ _ \/ __|/ _ \/ _ \/ _\` | / __| '_ \ "
+echo -n "\__ \ (__| | |_____| (_| |  __/ |_) |_____| |_) | | |  __/\__ \  __/  __/ (_| |_\__ \ | | |"; echo " $version"
+echo -n "|___/\___|_|        \__,_|\___|_.__/      | .__/|_|  \___||___/\___|\___|\__,_(_)___/_| |_|"; echo " $date"
+echo "                                          |_|                                             "
+echo ""
+
+
+echo "[CHECK FOR THE PRESENCE OF REQUIRED PACKAGES]"
+
+# Function to check if a package is installed
+
+is_installed() {
+    dpkg -l "$1" &> /dev/null
+    return $?
+}
+
+# List of required packages
+
+required_packages=(
+    "debconf-utils"
+    "xorriso"
+    "isolinux"
+    "gpg"
+    "libarchive-tools"
+)
+
+# Loop through the list and check if each package is installed
+
+for package in "${required_packages[@]}"; do
+    if is_installed "$package"; then
+        echo "$package is already installed." > /dev/null
+    else
+        echo "$package is not installed. Installing..."
+        sudo apt-get update && sudo apt-get install -y "$package"
+    fi
+done
+
+echo ''
+echo "All required packages are installed!"
+
+echo ''
+echo "[CHECK FOR THE PRESENCE OF REQUIRED FILES]"
+echo ''
+
+# List of required files
+
+required_files=(
+    "preseed.cfg"
+    "post-install.sh"
+    "SHA256SUMS"
+    "SHA256SUMS.sign"
+)
+
+# Check for a file that starts with "debian" and ends with ".iso"
+
+for file in debian*.iso; do
+    if [ -e "$file" ]; then
+        iso_file="$file"
+        break
+    fi
+done
+
+# Function to check if a file exists
+
+file_missing=false
+for file in "${required_files[@]}"; do
+    if [ ! -e "$file" ]; then
+        echo "$file is missing."
+        file_missing=true
+    fi
+done
+
+# Check if ISO file is found
+
+if [ -z "$iso_file" ]; then
+    echo "No file starting with 'debian' and ending with '.iso' found."
+    file_missing=true
+else
+    echo "ISO file found: $iso_file"
+fi
+
+# If any file is missing, display a summary and exit
+
+if [ "$file_missing" = true ]; then
+    echo "Some required files are missing. Please make sure the following files are present:"
+    for file in "${required_files[@]}"; do
+        echo "- $file"
+    done
+    echo "A file starting with 'debian' and ending with '.iso'"
+    read -n 1 -s -r -p "Press any key to exit..."
+    exit 1
+fi
+
+echo ''
+echo "All required files are present!"
+echo ''
+echo "[CREATE THE DIRECTORY STRUCTURE]"
+echo ''
+
+# Create the directory structure, removing any existing content
+
+if [ -d "$tmpdir/isofiles" ]; then
+    echo "Removing existing content in $tmpdir/isofiles"
+    sudo rm -rf "$tmpdir/isofiles/*"
+else
+    sudo mkdir -p "$tmpdir/isofiles"
+    echo "Directory structure created at $tmpdir/isofiles"
+fi
+
+echo ''
+echo "[INTEGRATION OF CUSTOMIZED FILES]"
+echo ''
+
+sudo bsdtar -C "$tmpdir/isofiles" -xf $iso_file 2> /dev/null
+
+sudo cp preseed.cfg "$tmpdir/isofiles/"
+sudo cp post-install.sh "$tmpdir/isofiles/"
+
+sudo chmod +w -R "$tmpdir/isofiles/install.amd/"
+sudo gunzip "$tmpdir/isofiles/install.amd/initrd.gz"
+echo "$tmpdir/isofiles/preseed.cfg" | sudo cpio -H newc -o -A -F "$tmpdir/isofiles/install.amd/initrd"
+sudo gzip "$tmpdir/isofiles/install.amd/initrd"
+sudo chmod -w -R "$tmpdir/isofiles/install.amd/"
+
+echo ''
+echo "[CREATING A CUSTOM GRUB MENU]"
+echo ''
+
+sudo tee "$tmpdir/isofiles/boot/grub/grub.cfg" > /dev/null << EOF
+set default=0
+set timeout=$grub_timeout
+
+# Define font and load it directly
+font=unicode
+loadfont $font
+
+# Set graphics mode and payload
+set gfxmode=800x600
+set gfxpayload=keep
+
+# Load necessary modules
+insmod efi_gop
+insmod efi_uga
+insmod video_bochs
+insmod video_cirrus
+insmod gfxterm
+insmod png
+
+# Set terminal output to graphical terminal
+terminal_output gfxterm
+
+# Load background image and set colors
+background_image /isolinux/splash.png || background_image /splash.png
+set color_normal=light-gray/black
+set color_highlight=white/black
+
+# Load and play a sound
+insmod play
+play 960 440 1 0 4 440 1
+
+# Set theme
+set theme=/boot/grub/theme/1
+
+# Menu entry
+menuentry "$grub_entry" {
+    linux    /install.amd/vmlinuz preseed/file=/cdrom/preseed.cfg locale=fr_FR.UTF-8 keymap=fr(latin9) language=fr country=FR autostrue --- quiet
+    initrd   /install.amd/initrd.gz
+}
+EOF
+
+echo "[MD5 HASH GENERATION]"
+echo ''
+
+sudo chmod +w "$tmpdir/isofiles/md5sum.txt"
+cd "$tmpdir/isofiles/"
+find -type f ! -name "md5sum.txt" -print0 | sudo xargs -0 md5sum | sudo tee md5sum.txt > /dev/null
+cd ../../
+sudo chmod -w "$tmpdir/isofiles/md5sum.txt"
+
+echo "[ISO FILE GENERATION]"
+echo ''
+
+sudo xorriso -as mkisofs \
+  -r \
+  -checksum_algorithm_iso md5,sha1,sha256,sha512 \
+  -V "$xorriso_v" \
+  -o "$xorriso_o" \
+  -isohybrid-mbr "/usr/lib/ISOLINUX/isohdpfx.bin" \
+  -b "isolinux/isolinux.bin" \
+  -c "isolinux/boot.cat" \
+  -boot-load-size 4 \
+  -boot-info-table \
+  -no-emul-boot \
+  -eltorito-alt-boot \
+  -e "boot/grub/efi.img" \
+  -no-emul-boot \
+  -isohybrid-gpt-basdat \
+  -isohybrid-apm-hfsplus \
+  "$tmpdir/isofiles/"
+
+# End of file
