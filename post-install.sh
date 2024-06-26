b5c146b67fb26077a779c94b41f4ace97a1cde39
Frédéric POUPET <126384795+fredericpoupet@users.noreply.github.com>
2024-06-26 20:45:14 +0200

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -13,7 +13,7 @@ hostname $HOSTNAME
 
 echo "$HOSTNAME" > /etc/hostname
 
-sed -i "s/^127.0.1.1[ \t].*/127.0.1.1\t$HOSTNAME/" /etc/hosts
+sudo sed -i "s/unassigned-hostname/$HOSTNAME/g" /etc/hosts
 
 # USERS MANAGEMENT
 
